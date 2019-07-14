496d05a744d602fa491bb48a14d466ff9680a686
Nazar Barchuk <nazar.barchuk@gmail.com>
2019-07-14 19:52:33 +0300

    install material ui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import configureStore, { history } from 'store/configureStore';
 import routes from 'routes/routes';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>{routes}</ConnectedRouter>
+    <ConnectedRouter history={history}>
+      <Fragment>
+        <CssBaseline />
+        {routes}
+      </Fragment>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
